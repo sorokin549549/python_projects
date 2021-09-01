b013399b2b355143e20b9bb62adc65d539b15541
sorokin549549 <mdU0VBBOVOeeW1AQegRrJuJ>
2021-09-01 14:58:41 +0300

    cesar_rus_part

--- cesar_project.py
+++ cesar_project.py
@@ -1,2 +1,24 @@
-x = int(input())
-print(x, '))))')
+y, x = 10, 'Блажен, кто верует, тепло ему на свете!'
+for i in range(0, int(len(x))):
+    m = ord(x[i])
+    #print(m)
+    if 1040 <= m <= 1071:
+        n = m + y
+        if n > 1071:
+            n = 1040 + (n - 1071)
+        v = (chr(n))
+        print(v, end='')
+
+    elif 1072 <= m <= 1103:
+        n = m + y
+        if n > 1103:
+            n = 1072 + (n - 1103)
+        v = (chr(n))
+        print(v, end='')
+    else:
+        print(x[i], end='')
+
+
+
+
+

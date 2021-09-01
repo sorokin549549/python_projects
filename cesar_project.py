a4dc0daaa0992ee9da0486d3aa3fa5c7a4e6c253
sorokin549549 <mdU0VBBOVOeeW1AQegRrJuJ>
2021-09-01 17:46:07 +0300

    cesar_ru_and_eng_decrypt

--- cesar_project.py
+++ cesar_project.py
@@ -7,7 +7,6 @@ def cesar_rus(y,x): #функция для кириллицы
                 n = 1039 + (n - 1071)
             v = (chr(n))
             print(v, end='')
-
         elif 1072 <= m <= 1103:    #условие для строчных букв
             n = m + y
             if n > 1103:
@@ -16,9 +15,6 @@ def cesar_rus(y,x): #функция для кириллицы
             print(v, end='')
         else:
             print(x[i], end='')
-
-
-
 def cesar_eng(y,x): #функция для латиницы
     for i in range(0, int(len(x))):
         m = ord(x[i])
@@ -29,7 +25,6 @@ def cesar_eng(y,x): #функция для латиницы
                 n = 64 + (n - 90)
             v = (chr(n))
             print(v, end='')
-
         elif 97 <= m <= 122:    #условие для строчных букв
             n = m + y
             if n > 122:
@@ -38,8 +33,52 @@ def cesar_eng(y,x): #функция для латиницы
             print(v, end='')
         else:
             print(x[i], end='')
+def cesar_rus_decrypt(y,x): #функция для кириллицы
+    for i in range(0, int(len(x))):
+        m = ord(x[i])
+        if 1040 <= m <= 1071:   #условие для заглавных букв
+            n = m - y
+            if n < 1040:
+                n = 1072 - (1040 - n)
+            v = (chr(n))
+            print(v, end='')
+        elif 1072 <= m <= 1103:    #условие для строчных букв
+            n = m - y
+            if n < 1072:
+                n = 1104 - (1072 - n)
+            v = (chr(n))
+            print(v, end='')
+        else:
+            print(x[i], end='')
+def cesar_eng_decrypt(y,x): #функция для латиницы
+    count = 0
+    for y in range(25):
+
+        print(count, sep='\n')
+        count += 1
+
+        for i in range(0, int(len(x))):
+            m = ord(x[i])
+
+            if 65 <= m <= 90:   #условие для заглавных букв
+                n = m - y
+                if n < 65:
+                    n = 91 - (65 - n)
+                v = (chr(n))
+                print(v, end='')
+            elif 97 <= m <= 122:    #условие для строчных букв
+                n = m - y
+                if n < 97:
+                    n = 123 - (97 - n)
+                v = (chr(n))
+                print(v, end='')
+            else:
+                print(x[i], end='')
+
 y, x = int(input()), input()
-cesar_eng(y, x)
+cesar_eng_decrypt(y,x)
+
+
 
 
 

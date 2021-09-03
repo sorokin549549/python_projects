8017840dbc9bdd4a65f87ee62cff78623b8b9908
sorokin549549 <mdU0VBBOVOeeW1AQegRrJuJ>
2021-09-03 17:50:40 +0300

    adv_course

--- trash.py
+++ trash.py
@@ -1,23 +1,8 @@
-def cesar_eng(x): #функция для латиницы
-    for j in range(0, len(x)):
+num = int(input())
+bin_num = bin(num)  # 0b1111111
+oct_num = oct(num)  # 0o177
+hex_num = hex(num).upper()  # 0x7f
 
-        for i in range(0, (len(x[j]))):
-            m = ord(x[j][i])
-            #print(m)
-            if 65 <= m <= 90:   #условие для заглавных букв
-                n = m + len(x[i])
-                if n > 90:
-                    n = 64 + (n - 90)
-                v = (chr(n))
-                print(v, end='')
-            elif 97 <= m <= 122:    #условие для строчных букв
-                n = m + len(x[i])
-                if n > 122:
-                    n = 96 + (n - 122)
-                v = (chr(n))
-                print(v, end='')
-            else:
-                print(x[i], end='')
-
-x = input().split()
-cesar_eng(x)
+print(bin_num[2:])  # 1111111
+print(oct_num[2:])  # 177
+print(hex_num[2:])

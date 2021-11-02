50ccd93da9fe6da0f8cc7784dbf5547f60c60639
sorokin549 <nominaltechnopso@gmail.com>
2021-11-02 22:51:32 +0300

    commit_4.6.10_1

--- Advanced course/4. Nested lists/4.6/4.6.10.py
+++ Advanced course/4. Nested lists/4.6/4.6.10.py
@@ -7,22 +7,58 @@ high = m - 1
 low = 0
 count = int((n+1)/2)
 
+if m == n:
+    for i in range(count):
+        for j in range(low, high+1):
+            matrix[i][j] = x
+            x+=1
+        for j in range(low+1, high+1):
+            matrix[j][high] = x
+            x+=1
+        for j in range(high-1, low-1, -1):
+            matrix[high][j] = x
+            x+=1
+        for j in range(high-1, low, -1):
+            matrix[j][low] = x
+            x+=1
+        low = low+1
+        high =high-1
 
-for i in range(count):
-    for j in range(low, high+1):
-        matrix[i][j] = x
-        x+=1
-    for j in range(low+1, high+1):
-        matrix[j][high] = x
-        x+=1
-    for j in range(high-1, low-1, -1):
-        matrix[high][j] = x
-        x+=1
-    for j in range(high-1, low, -1):
-        matrix[j][low] = x
-        x+=1
-    low = low+1
-    high =high-1
+elif n > m: # 4 3
+    print('suka')
+    for i in range(count):
+        for j in range(low, high+1):
+            matrix[i][j] = x
+            x+=1
+        for j in range(low+1, high+1):
+            matrix[j][high] = x
+            x+=1
+        for j in range(high, low-1, -1):
+            matrix[high+1][j] = x
+            x+=1
+        for j in range(high, low, -1):
+            matrix[j][low] = x
+            x+=1
+        low = low+1
+        high =high-1
+elif n < m: # 3 4
+    print('blya')
+    for i in range(count):
+        for j in range(low, high+1):
+            matrix[i][j] = x
+            x+=1
+        for j in range(low, high-1):
+            matrix[j][high] = x
+            x+=1
+        for j in range(high, low, -1):
+            matrix[high-1][j] = x
+            x+=1
+        for j in range(high-1, low, -1):
+            matrix[j][low] = x
+            x+=1
+        low = low+1
+        high =high-1
+
 
 for i in range(n):
     for j in range(m):
---
+++ trash2.py
@@ -0,0 +1,49 @@
+n, m = [int(i) for i in input().split()]
+
+matrix = [[0] * m for _ in range(n)]
+
+x = 1
+high = m - 1
+low = 0
+count = int((n+1)/2)
+
+
+
+
+
+# for i in range(count):
+#     for j in range(low, high+1):
+#         matrix[i][j] = x
+#         x+=1
+#     for j in range(low+1, high+1+(2-5)):
+#         matrix[j][high] = x
+#         x+=1
+#     for j in range(high, low-1, -1):
+#         matrix[high+1][j] = x
+#         x+=1
+#     for j in range(high-(2-5), low, -1):
+#         matrix[j][low] = x
+#         x+=1
+#     low = low+1
+#     high =high-1
+
+for i in range(count):
+    for j in range(low, high+1):
+        matrix[i][j] = x
+        x+=1
+    for j in range(low+1, high+1+(n-m)):
+        matrix[j][high] = x
+        x+=1
+    for j in range(high-(n-m), low-1, -1):
+        matrix[high+1][j] = x
+        x+=1
+    for j in range(high-(n-m), low, -1):
+        matrix[j][low] = x
+        x+=1
+    low = low+1
+    high =high-1
+
+for i in range(n):
+    for j in range(m):
+        print(str(matrix[i][j]).ljust(3), end=' ')
+    print()
